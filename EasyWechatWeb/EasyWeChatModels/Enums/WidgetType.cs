8e17d22c7f075687b953dc3bf35ef7a6769189ec
李林 <565387073@qq.com>
2026-05-14 19:04:48 +0800

    WidgetType enum
    Co-Authored-By: lilin <565387073@qq.com>

---
+++ EasyWechatWeb/EasyWeChatModels/Enums/WidgetType.cs
@@ -0,0 +1,16 @@
+namespace EasyWeChatModels.Enums;
+
+/// <summary>
+/// 桌面组件类型枚举
+/// </summary>
+public enum WidgetType
+{
+    /// <summary>统计卡片</summary>
+    Card = 1,
+    /// <summary>数据列表</summary>
+    List = 2,
+    /// <summary>图片展示</summary>
+    Image = 3,
+    /// <summary>图表统计</summary>
+    Chart = 4
+}
